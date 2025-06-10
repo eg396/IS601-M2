8369dd78eea47aae98d8752da62867f9295df9fd
Evan <eg396@njit.edu>
2025-06-09 22:14:10 -0400

    feat: imported essential files and added basic calculator functionality

---
+++ tests/test_operations.py
@@ -0,0 +1,13 @@
+from app.operations import addition, subtraction, multiplication, division
+
+def test_addition():
+    assert addition(1, 1) == 2
+
+def test_subtraction():
+    assert subtraction(1, 1) == 0
+
+def test_multiplication():
+    assert multiplication(2, 3) == 6
+
+def test_division():
+    assert division(4, 2) == 2
